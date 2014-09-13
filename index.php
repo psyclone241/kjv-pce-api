6e1cab3275f16ab336b665b2e58000e244415f00
Roy Keyes <keyes@ufl.edu>
2014-09-13 01:12:10 -0400

    Adding OT/NT section search. Adding updated README.  Some other stuff I can't remember because it is really late.

--- index.php
+++ index.php
@@ -125,6 +125,9 @@
                     $book_query = "BookAbr='" . $book . "'";
                 }
             }
+            if($and) {
+                $query .= " AND ";
+            }
             $query .= $book_query;
             $and = true;
         }
