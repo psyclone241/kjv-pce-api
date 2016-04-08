97f34ee32f31b855f015f11594140e40e440f40f
Roy Keyes <psyclone241@users.noreply.github.com>
2016-04-08 12:51:03 -0400

    Update the setup script to allow skipping virtualenv and installing the bower_components

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
-cp config.example.py config.py
-virtualenv venv
+VENV=$1
 
-venv/bin/pip install flask
-venv/bin/pip install flask-sqlalchemy
-venv/bin/pip install flask-marshmallow
-venv/bin/pip install marshmallow-sqlalchemy
+cp config.example.py config.py
+cp app/static/mod_bible/config.example.js app/static/mod_bible/config.js
+
+if [ ! -z "${VENV}"];
+then
+  virtualenv venv
+  
+  venv/bin/pip install flask
+  venv/bin/pip install flask-sqlalchemy
+  venv/bin/pip install flask-marshmallow
+  venv/bin/pip install marshmallow-sqlalchemy
+fi
 
 edit config.py
+
+cd app/static/resources/
+bower install
+cd ../mod_bible/
+edit config.js
